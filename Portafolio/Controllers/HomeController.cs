374638ee9dd001993f30e842a4d0cb1fdb5939f0
Wilson Bautista Cuevas <wbautista@aportesenlinea.com>
2022-05-05 09:53:38 -0500

    Primer commit del proyecto

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using Portafolio.Servicios;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRepositorioProyectos repositorioProyectos;
+        
+        public HomeController(
+            IRepositorioProyectos repositorioProyectos
+
+            )
+        {
+            this.repositorioProyectos = repositorioProyectos;
+            
+        }
+
+        public IActionResult Index()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
+           
+            var modelo = new HomeIndexViewModel() { 
+                Proyectos = proyectos
+            };
+            return View(modelo);
+        }
+
+        public IActionResult Proyectos()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+
+            return View(proyectos);
+        }
+
+        [HttpGet]
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
+        {
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Portafolio/Models/ContactoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Portafolio.Models
+{
+    public class ContactoViewModel
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Mensaje { get; set; }
+
+    }
+}
